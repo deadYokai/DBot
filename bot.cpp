3feba47b5623d2f34bba2c1bd8befc09f739120c
root <root@vps.server.local>
2020-06-23 13:26:10 +0000

    Build 230620

--- bot.cpp
+++ bot.cpp
@@ -3,32 +3,50 @@
 #include <fstream>
 #include <sstream>
 #include <map>
+#include <curl/curl.h>
 
 using namespace std;
 
 void launch(){
 	cout << "Bot token checking...\n";
 	string token;
-	string line;
   	ifstream myfile ("bot_token");
 	if (myfile.is_open())
 	{
 		cout << "Authorize as ";
-	    while ( getline (myfile,line) )
-	    {
-	      cout << line << '\n';
-	    }
+		getline (myfile,token);
+		cout << token;
+		cout << "\n\n";
 		myfile.close();
 	}else{  
-	    cout << "Bot Token not found.\n";
-	    cout << "Enter Bot Token: ";
-	    getline (cin, token);
-	    ofstream myfile;
+		cout << "Bot Token not found.\n";
+		cout << "Enter Bot Token: ";
+		getline (cin, token);
+		ofstream myfile;
 		myfile.open ("bot_token");
 		myfile << token;
 		myfile.close();
-	    cout << "New token:"<< token <<"\n\n";
-    }
+		cout << "New token:"<< token <<"\n\n";
+    	}
+	CURL *curl;
+	CURLcode res;
+	curl = curl_easy_init();
+	struct curl_slist *headers = NULL;
+	if(curl) {
+		headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)");
+		headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+		string a = "Authorization: Bot " + token;
+		headers = curl_slist_append(headers, a.c_str());
+		curl_easy_setopt(curl, CURLOPT_URL, "https://discordapp.com/api/channels/721018480761110618/messages");
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		curl_easy_setopt(curl, CURLOPT_POST, 1);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "content=Если вы заблудились и очень устали - позовите кровососа\nУсталость как рукой снимет");
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+		cout << res << "\n";
+                cout << "Headers:" << headers << "\n";
+
+	}
 
     // TODO
 }
